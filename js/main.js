2cb353dcbf9859b2a186e95ac0db5a53542c73cf
justxiaoyizhi <18702528592@163.com>
2016-12-31 00:43:23 +0800

    修改手机上出现滑动列表

--- js/main.js
+++ js/main.js
@@ -1,12 +1,10 @@
 /**
  * Created by Xiao on 2016/12/7.
  */
-$.material.init();
-
-/**
- * 侧边栏插件
- */
-$("#menu-toggle").click(function(e) {
-    e.preventDefault();
-    $("#wrapper").toggleClass("toggled");
-});
+$('.button-collapse').sideNav({
+        menuWidth: 300, // Default is 240
+        edge: 'left', // Choose the horizontal origin
+        closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+        draggable: true // Choose whether you can drag to open on touch screens
+    }
+);
