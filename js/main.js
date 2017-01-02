7632b24fc5d03347bd4f57118b3b9e210b8e59f1
justxiaoyizhi <18702528592@163.com>
2017-01-02 14:48:57 +0800

    首页完成，滑动内容未完成

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 /**
  * Created by Xiao on 2016/12/7.
  */
+
+/* 左侧滑动菜单 */
 $('.button-collapse').sideNav({
         menuWidth: 300, // Default is 240
         edge: 'left', // Choose the horizontal origin
@@ -8,3 +10,15 @@ $('.button-collapse').sideNav({
         draggable: true // Choose whether you can drag to open on touch screens
     }
 );
+
+/* 切换正反话题时 */
+var switchChanged = true;
+$('#oppositeSwitch').on('change', function (data) {
+    switchChanged = !switchChanged;
+    // false 为反方
+    if(!switchChanged) {
+        Materialize.toast('反方话题', 1500)
+    } else {
+        Materialize.toast('正方话题', 1500)
+    }
+});
---
+++ js/timer.js
@@ -0,0 +1,40 @@
+/**
+ * Created by Xiao on 2017/1/1.
+ */
+var nextDataNumber = 5;
+var ulNode = $('ul.timeline');
+
+/*function initLiNodes() {
+    var liNodes = ulNode.find('li'), count = liNodes.length, i, liNode, leftCount = nextDataNumber * 20;
+    for (i = 0; i < count; i++) {
+        liNode = $(liNodes.get(i));
+        if (i % 2 !== 0) {
+            liNode.addClass('alt');
+        } else {
+            liNode.removeClass('alt');
+        }
+        liNode.find('.number').text(leftCount + count - i);
+    }
+}*/
+
+
+/*
+$('#fetchNextData').click(function () {
+    var $this = $(this);
+    $this.addClass('disabled').text('......');
+
+    $.get('./version_data_' + nextDataNumber + '.txt', function (data) {
+        ulNode.append(data);
+        $this.removeClass('disabled').text('后二十条数据');
+        nextDataNumber--;
+
+        if (nextDataNumber === 0) {
+            $this.hide();
+        }
+
+        initLiNodes();
+    });
+
+});
+ */
+/*initLiNodes();*/
