77ee3cc8377d605a6e65cf0339f7e92a69d9a736
justxiaoyizhi <18702528592@163.com>
2017-03-12 04:09:20 +0800

    新增回答问题的样式

---
+++ js/answer.js
@@ -0,0 +1,7 @@
+/**
+ * Created by Administrator on 2017/3/12.
+ */
+$(".answer-box .answer-footer .action").click(function() {
+    $(this).toggleClass("action");
+    $(this).toggleClass("action-active");
+});
