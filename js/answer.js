cc488ce812b2dfa3802e1050e9ca845f79aa2b19
justxiaoyizhi <18702528592@163.com>
2017-03-12 19:44:58 +0800

    回答问题的样式初步完成

--- js/answer.js
+++ js/answer.js
@@ -1,7 +1,18 @@
 /**
  * Created by Administrator on 2017/3/12.
  */
+var editor;
+$(function() {
+    editor = new Simditor({
+        textarea: $('#editor')
+    });
+});
+
 $(".answer-box .answer-footer .action").click(function() {
     $(this).toggleClass("action");
     $(this).toggleClass("action-active");
 });
+
+$(document).on("click","#submit-answer",function() {
+    alert(editor.getValue());
+})
